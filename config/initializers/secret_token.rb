ea1e5adee829648458ed00ae805660ae68458a23
Imran Sulemanji <imranolas@gmail.com>
2014-03-12 16:11:39 +0000

    New rails app. Adding rspec, guard, capybara and devise. Setup testing environment '

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Attend::Application.config.secret_token = '4a01c73f212ae2111575026388cd47b8b88b3881cc5ae1361bb10a61fc1390fc6b03e32cf082f4fa21dec1bd9c9263b4d110acf491122bc5a98a6516b77ea4e8'
