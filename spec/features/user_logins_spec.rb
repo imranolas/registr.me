94a727e2baf50cd417e533444d70f6a794ef0b16
Imran Sulemanji <imranolas@gmail.com>
2014-03-14 10:03:04 +0000

    adding further tests

--- spec/features/user_logins_spec.rb
+++ spec/features/user_logins_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe "UserSignup" do
   it 'should create users without a password' do
-    visit '/users/sign_up'
+    visit '/'
     fill_in 'Email', with: 'foo@bar.com'    
     expect { click_button 'Sign up' }.to change { User.count }.by(1)
   end
@@ -10,7 +10,7 @@ describe "UserSignup" do
   context "User confirmation email" do
     before do
       clear_emails
-      visit '/users/sign_up'
+      visit '/'
       fill_in 'Email', with: 'foo@bar.com'
       click_button 'Sign up'
       open_email('foo@bar.com')
@@ -18,27 +18,15 @@ describe "UserSignup" do
 
     it { current_email.should_not be_nil }
 
-    it 'confirm account' do
-      pending
+    it 'Account confirmation' do
       current_email.click_link 'Confirm'
       page.should have_content "almost done"
       fill_in 'name', with: 'foo bar'
       fill_in 'user[password]', with: 'password'
       fill_in 'user[password_confirmation]', with: 'password'
-      click_button 'Confirm Account'
-      save_and_open_page
-
       expect { click_button 'Confirm Account' }.to change { Teacher.count }.by(1)
       User.last.should be_confirmed
     end 
-
-
+    
   end
-
-
-
-  it 'should create a new teacher record' do
-
-  end
-
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,9 +6,17 @@ describe User do
   
   context 'without password' do
     subject(:user) { create(:user_without_password) }
+    it { should be_valid }
     it { expect{ create(:user_without_password) }.to change { User.count }.by(1) }
     it { should_not be_confirmed }
     its(:confirmation_token) { should_not be_nil }
+  end
+
+  context 'with password' do
+    subject(:user) { create(:user) }
+    it { should be_valid }
+    its(:password) { should_not be_nil }
+    
   end
 
 
