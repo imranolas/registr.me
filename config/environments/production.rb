60248152ba219958ed6f331aea82349bccbfe2f9
Imran Sulemanji <imranolas@gmail.com>
2014-04-13 14:34:02 +0100

    Update email settings for heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -21,9 +21,14 @@ Attend::Application.configure do
   config.assets.digest = true
   config.action_mailer.default_url_options = { :host => 'registr.herokuapp.com' }
   
+  ActionMailer::Base.delivery_method = :smtp
+  ActionMailer::Base.perform_deliveries = true
+  ActionMailer::Base.raise_delivery_errors = true
+    
   ActionMailer::Base.smtp_settings = {
   :address        => 'smtp.gmail.com',
-  :port           => '465',
+  :port               => 587,
+  :domain             => 'gmail.com',
   :authentication => :plain,
   :user_name      => ENV['GMAIL_USERNAME'],
   :password       => ENV['GMAIL_PASSWORD'],
